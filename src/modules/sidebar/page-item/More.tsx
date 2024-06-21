87afdf5d7d7fbb59b77fff6e0ea9418b1ffd2533
Hung Nguyen <hung.workplace@gmail.com>
2024-06-21 15:19:23 +0700

    fix hover delete more button

--- src/modules/sidebar/page-item/More.tsx
+++ src/modules/sidebar/page-item/More.tsx
@@ -63,7 +63,7 @@ export default function More({ onEditable, className }: MoreProps) {
           </div>
 
           <AlertDialog>
-            <AlertDialogTrigger className="hover:bg-[#f8f8f8] hover:text-red-500 rounded-md px-3 py-2 cursor-pointer">
+            <AlertDialogTrigger className="hover:bg-[#f8f8f8] hover:text-red-500 w-full text-left rounded-md px-3 py-2 cursor-pointer">
               Delete
             </AlertDialogTrigger>
 
